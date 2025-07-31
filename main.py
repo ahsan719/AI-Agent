e6a14a0af92c579ebbcbc3066da859ff997dc2bd
Ahsan Naseer <ahsannilore@gmail.com>
2025-07-31 14:33:24 +0500

    Lllm setup

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_openai import ChatOpenAI
-
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 load_dotenv()
 
-llm =ChatOpenAI(model="gpt-3.5-turbo")
+class ResearchResponse(BaseModel):
+    topic: str
+    summary :str 
+    Soruces: list[str]
+    tools_used : list[str]
+
+llm = ChatGroq( model="llama3-8b-8192")
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
